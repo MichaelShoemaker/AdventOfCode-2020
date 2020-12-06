cdf9dfcd27b15514bee7e9772de2242db5d2362c
Michael Shoemaker <mikeshoemaker2000@yahoo.com>
2020-12-06 12:08:04 -0600

    Update Part2.py
    Updated function name to follow PEP style guide.

--- Day1/Part2.py
+++ Day1/Part2.py
@@ -3,12 +3,12 @@ with open(r'input.txt','r') as infile:
     for line in infile:
         inputs.append(int(line))
 
-def sumIs2020(x):
+def sum_is_2020(x):
     for l,n in enumerate(x):
         for m,o in enumerate(x):
             for y,z in enumerate(x):
                 if l!=m and m!=y and y!=l and n + o + z == 2020:
                     return(n*o*z)
         
-ans = sumIs2020(inputs)
+ans = sum_is_2020(inputs)
 print(ans)
