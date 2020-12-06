4ea7daad13297e96cc3e6e26c79c399fc9074889
Michael Shoemaker <mikeshoemaker2000@yahoo.com>
2020-12-06 12:09:33 -0600

    Update Part1.py
    Updated function and variable names to follow PEP style guide.

--- Day2/Part1.py
+++ Day2/Part1.py
@@ -4,11 +4,11 @@ with open(r'input.txt','r') as infile:
               inputs.append(line)
           
 
-def isValid(pws):
+def is_valid(pws):
     valids = 0
     
     for line in pws:
-        charCount = 0
+        char_count = 0
         pieces = line.split(':')
         numLets = pieces[0].split(' ')
         bottom = int(numLets[0].split('-')[0])
@@ -16,11 +16,11 @@ def isValid(pws):
         letter = numLets[1]
         for i in pieces[1]:
             if i == letter:
-                charCount+=1
-        if charCount >= bottom and charCount <= top:
+                char_count+=1
+        if char_count >= bottom and char_count <= top:
             valids+=1
 
     return valids
 
-numValids = isValid(inputs)
-print(numValids)
+num_valids = is_valid(inputs)
+print(num_valids)
